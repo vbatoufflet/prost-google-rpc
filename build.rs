d4adef6d2b793ed1854c2ad007e501a6d66b019c
Vincent Batoufflet <vincent@batou.dev>
2025-12-28 13:34:45 +0100

    chore: update Cargo dependencies

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(false)
         .build_client(false)
         .compile_protos(&["google/rpc/status.proto"], &["proto"])
